18eeadb8a1c111af89874453f1ee6354a658610d
Myjem <siepkawiktor240@gmail.com>
2024-06-18 16:42:11 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,26 +3,39 @@
 #include "ai.h"
 
 int main() {
-    int size, winLength;
+    int size, winLength, depth;
     std::cout << "Podaj rozmiar planszy: ";
     std::cin >> size;
+    if(size <= 1)
+    {
+        std::cout << "Za maly rozmiar tablicy";
+        return 1;
+    }
     std::cout << "Podaj wymagana ilosc znakow w jednym ciagu aby wygrac: ";
     std::cin >> winLength;
 
-    if (winLength > size) {
+    if (winLength > size || winLength < 0) {
         std::cerr << "Wymagana ilosc znaków nie moze byc wieksza niz rozmiar planszy" << std::endl;
         return 1;
     }
+    std::cout << "Podaj glebokosc ai";
+    std::cin >> depth;
+    if(depth <= 0)
+    {
+        std::cout << "Za niska glebokoc" << std::endl;
+        return 1;
+    }
+    
 
     Board board(size, winLength);
-    AI ai('O', 'X', 10); // AI- O, Człowiek - X
+    AI ai('O', 'X', depth); // AI- O, Człowiek - X
 
     char currentPlayer = 'X';
     while (true) {
         board.display();
         if (currentPlayer == 'X') {
             int row, col;
-            std::cout << "Gracz " << currentPlayer << ", podaj wiersz i kolumnę: ";
+            std::cout << "Gracz " << currentPlayer << ", podaj wiersz i kolumne: ";
             std::cin >> row >> col;
 
             if (board.makeMove(row, col, currentPlayer)) {
@@ -39,7 +52,7 @@ int main() {
                 }
                 currentPlayer = 'O';
             } else {
-                std::cout << "Nieprawidłowy ruch, spróbuj ponownie." << std::endl;
+                std::cout << "Nieprawidlowy ruch, sprobuj ponownie." << std::endl;
             }
         } else {
             ai.makeMove(board);
